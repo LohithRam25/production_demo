e06645bb26a7d95fcd5bc4769fb4f404a204ee60
ram-25082005 <rampalla2005@gmail.com>
2025-02-13 21:15:20 +0530

    second

--- production_demo/settings.py
+++ production_demo/settings.py
@@ -32,7 +32,7 @@ if ENVIRONMENT =='development':
 else:
     DEBUG=False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
